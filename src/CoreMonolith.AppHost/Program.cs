91f0ca7c2e7d8b17c4e3e5803063622405fd19b0
zeox115 <zeox115@gmail.com>
2024-12-09 20:29:01 +0100

    aspire tweaks

--- src/CoreMonolith.AppHost/Program.cs
+++ src/CoreMonolith.AppHost/Program.cs
@@ -1,17 +1,24 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("core-monolith-db")
-    .WithDataVolume()
-    .WithPgAdmin();
+var corePostgresName = builder.Configuration["AppConfig:CorePostgresName"];
+var coreMqName = builder.Configuration["AppConfig:CoreRabbitMqName"];
+var coreWebApiName = builder.Configuration["AppConfig:CoreWebApiName"];
 
-//var coreMonolithdb = postgres.AddDatabase("core-monolith");
+var baseDataVolumePath = builder.Configuration["Containers:DataVolumePath"];
 
-var rabbitMq = builder.AddRabbitMQ("core-monolith-mq")
-    .WithManagementPlugin();
+var postgres = builder.AddPostgres($"{corePostgresName}")
+    .WithDataBindMount(@$"{baseDataVolumePath}\{corePostgresName}")
+    .WithPgAdmin()
+    .WithLifetime(ContainerLifetime.Persistent);
 
-builder.AddProject<Projects.CoreMonolith_WebApi>("core-monolith-webapi")
+var rabbitMq = builder.AddRabbitMQ($"{coreMqName}")
+    .WithDataBindMount(@$"{baseDataVolumePath}\{coreMqName}")
+    .WithManagementPlugin()
+    .WithLifetime(ContainerLifetime.Persistent);
+
+builder.AddProject<Projects.CoreMonolith_WebApi>($"{coreWebApiName}")
     .WithReference(postgres)
-    .WithReference(rabbitMq);
+    .WithReference(rabbitMq)
+    .WaitFor(postgres);
 
 builder.Build().Run();
-

7876b08601ae04930b8bcb4411bfab970d635b50
zeox115 <zeox115@gmail.com>
2024-12-09 23:46:54 +0100

    some more config updates

--- src/CoreMonolith.AppHost/Program.cs
+++ src/CoreMonolith.AppHost/Program.cs
@@ -4,6 +4,7 @@ var postgresUser = builder.AddParameter("core-monolith-db-username");
 var postgresPassword = builder.AddParameter("core-monolith-db-password");
 
 var corePostgresName = builder.Configuration["AppConfig:CorePostgresName"];
+var corePostgresDbName = builder.Configuration["AppConfig:CorePostgresDbName"];
 var coreMqName = builder.Configuration["AppConfig:CoreRabbitMqName"];
 var coreWebApiName = builder.Configuration["AppConfig:CoreWebApiName"];
 var baseDataVolumePath = builder.Configuration["Containers:DataVolumePath"];
@@ -11,7 +12,8 @@ var baseDataVolumePath = builder.Configuration["Containers:DataVolumePath"];
 var postgres = builder.AddPostgres($"{corePostgresName}", postgresUser, postgresPassword)
     .WithDataBindMount(@$"{baseDataVolumePath}\{corePostgresName}")
     .WithPgAdmin()
-    .WithLifetime(ContainerLifetime.Persistent);
+    .WithLifetime(ContainerLifetime.Persistent)
+    .AddDatabase($"{corePostgresDbName}");
 
 var rabbitMq = builder.AddRabbitMQ($"{coreMqName}")
     .WithDataBindMount(@$"{baseDataVolumePath}\{coreMqName}")

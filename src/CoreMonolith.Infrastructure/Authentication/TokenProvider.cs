c237f1a8ee0fa4087b65a21e03d689a3f1f05a1f
zeox115 <zeox115@gmail.com>
2024-12-12 18:24:42 +0100

    authentication seems to be working

--- src/CoreMonolith.Infrastructure/Authentication/TokenProvider.cs
+++ src/CoreMonolith.Infrastructure/Authentication/TokenProvider.cs
@@ -23,6 +23,7 @@ internal sealed class TokenProvider(IConfiguration configuration) : ITokenProvid
             Subject = new ClaimsIdentity(
             [
                 new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim("userid", user.Id.ToString()),
                 new Claim(JwtRegisteredClaimNames.Email, user.Email)
             ]),
             Expires = DateTime.UtcNow.AddMinutes(configuration.GetValue<int>("Jwt:ExpirationInMinutes")),

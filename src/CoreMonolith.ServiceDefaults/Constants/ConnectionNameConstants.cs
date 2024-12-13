ff70026bb26fc0441e92cf399145fc01364fff44
zeox115 <zeox115@gmail.com>
2024-12-13 02:11:56 +0100

    oops

--- src/CoreMonolith.ServiceDefaults/Constants/ConnectionNameConstants.cs
+++ src/CoreMonolith.ServiceDefaults/Constants/ConnectionNameConstants.cs
@@ -3,7 +3,7 @@
 public static class ConnectionNameConstants
 {
     public static string DbConnStringName => "core-monolith-db";
-    public static string RedisConnectionName => "core-monolith-redisb";
+    public static string RedisConnectionName => "core-monolith-redis";
     public static string RabbitMqConnectionName => "core-monolith-mq";
     public static string WebApiConnectionName => "core-monolith-webapi";
     public static string WebAppConnectionName => "download-manager-webapp";
